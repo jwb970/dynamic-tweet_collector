a13c7142e5c23e94bea2c4cc551e414bda0b804b
rkempter <renato.kempter@gmail.com>
2014-06-13 17:53:13 +0400

    First draft

---
+++ dynamic-hashtag/config.py
@@ -0,0 +1,28 @@
+"""
+Configuration file for dynamic hashtag
+"""
+
+
+# MySQL access data
+MYSQL_HOST = ""
+MYSQL_USER = ""
+MYSQL_PWD = ""
+MYSQL_DB = ""
+
+# Parameters for Dynamic Hashtag
+WINDOW_SIZE = 20
+REMODEL = 4
+TOP_K = 30
+LANGUAGE = ['english']
+
+# SVM
+
+
+# TWITTER API SETTINGS
+CONSUMER_SECRET = ""
+CONSUMER_KEY = ""
+
+ACCESS_TOKEN = ""
+ACCESS_TOKEN_SECRET = ""
+
+
